704324f75003837d1a5c0350e451ab7ed4d80b56
김수빈 <77378559+ksubin-dev@users.noreply.github.com>
2026-04-06 19:45:10 +0900

    feat: Domain 계층 Book 모델 및 Repository 인터페이스 설계
    core 패키지에 DataResult 래퍼 추가

---
+++ app/src/main/java/com/subin/cleanbookstore/domain/model/Book.kt
@@ -0,0 +1,12 @@
+package com.subin.cleanbookstore.domain.model
+
+data class Book(
+    val id: String,
+    val title: String,
+    val subtitle: String,
+    val authors: List<String>,
+    val publisher: String,
+    val description: String,
+    val imageUrl: String,
+    val buyLink: String
+)
